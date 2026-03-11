072587bad85c77d59034f0259592279292dc8506
govgovnahr <govindn3@illinois.edu>
2026-03-11 16:16:53 -0700

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react'
 import './App.css';
-import InputComponent from './components/InputComponent';
 // import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import ChatWindowComponent from './components/ChatWindowComponent';
 import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
